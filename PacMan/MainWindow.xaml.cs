e5ff0413152c780e7021c83601aab94923f5cc9c
Mubarak <12859241+mcmubby@users.noreply.github.com>
2021-07-22 14:57:16 +0100

    Level 1 - Completed

--- PacMan/MainWindow.xaml.cs
+++ PacMan/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace PacMan
     {
 
         DispatcherTimer gameTime = new DispatcherTimer();
-        Stopwatch timer = new Stopwatch();
+        DateTime levelIime = DateTime.UtcNow + TimeSpan.FromMinutes(5);
 
         bool goLeft, goRight, goDown, goUp;
         bool noLeft, noRight, noDown, noUp;
@@ -34,7 +34,7 @@ namespace PacMan
         Rect pacmanHitBox;
 
         int ghostSpeed = 10;
-        int ghostMoveStep = 160;
+        int ghostMoveStep = 320;
         int currentGhostStep;
         int score = 0;
 
@@ -95,7 +95,7 @@ namespace PacMan
             gameTime.Tick += GameLoop;
             gameTime.Interval = TimeSpan.FromMilliseconds(20);
             gameTime.Start();
-            timer.Start();
+            //timer.Start();
 
             currentGhostStep = ghostMoveStep;
 
@@ -105,21 +105,22 @@ namespace PacMan
 
             ImageBrush redGhost = new ImageBrush();
             redGhost.ImageSource = new BitmapImage(new Uri(@"assets/red.jpg", UriKind.Relative));
-            redGuy.Fill = redGuy1.Fill = redGhost;
+            rightM3.Fill = leftM1.Fill = redGhost;
 
             ImageBrush pinkGhost = new ImageBrush();
             pinkGhost.ImageSource = new BitmapImage(new Uri(@"assets/pink.jpg", UriKind.Relative));
-            pinkGuy.Fill = pinkGuy1.Fill = pinkGhost;
+            leftM2.Fill = rightM1.Fill = pinkGhost;
 
             ImageBrush orangeGhost = new ImageBrush();
             orangeGhost.ImageSource = new BitmapImage(new Uri(@"assets/orange.jpg", UriKind.Relative));
-            orangGuy.Fill = orangGuy1.Fill = orangeGhost;
+            downM1.Fill = rightM2.Fill = orangeGhost;
         }
 
         private void GameLoop(object sender, EventArgs e)
         {
             Score.Content = "Score : " + score.ToString();
-            Time.Content = "Time : " + timer.Elapsed.ToString(@"mm\:ss");
+            //Time.Content = "Time : " + timer.Elapsed.ToString(@"mm\:ss");
+            Time.Content = "Time : " + (levelIime - DateTime.UtcNow).ToString(@"mm\:ss");
 
 
             //Pacman movement
@@ -142,6 +143,7 @@ namespace PacMan
             }
 
             pacmanHitBox = new Rect(Canvas.GetLeft(pacman), Canvas.GetTop(pacman), pacman.Width, pacman.Height);
+            var downM1HitBorder = new Rect(Canvas.GetLeft(downM1), Canvas.GetTop(downM1), downM1.Width, downM1.Height);
 
             foreach (var x in Level1Canvas.Children.OfType<Rectangle>())
             {
@@ -170,7 +172,13 @@ namespace PacMan
                         noDown = true;
                         goDown = false;
                     }
-                    
+
+                    //trial
+                    if (downM1HitBorder.IntersectsWith(hitBox))
+                    {
+                        Canvas.SetTop(downM1, Canvas.GetTop(downM1) - ghostSpeed);
+                    }
+
                     if (goUp == true && pacmanHitBox.IntersectsWith(hitBox))
                     {
                         Canvas.SetTop(pacman, Canvas.GetTop(pacman) + 10);
@@ -178,6 +186,59 @@ namespace PacMan
                         goUp = false;
                     }
                 }
+
+                if((string)x.Tag == "coin" && pacmanHitBox.IntersectsWith(hitBox) && x.Visibility == Visibility.Visible)
+                {
+                    x.Visibility = Visibility.Hidden;
+                    score++;
+                }
+
+                /*if ((string)x.Tag == "ghost")
+                {
+                    if (pacmanHitBox.IntersectsWith(hitBox))
+                    {
+                        GameOver($"Ghosts got you. \nScore: {score}. \nClick ok to go to main menu");
+                    }
+
+                    // Ghost movement 
+                    if (x.Name.Contains("left"))
+                    {
+                        Canvas.SetLeft(x, Canvas.GetLeft(x) - ghostSpeed);
+
+                    }
+
+                    if (x.Name.Contains("right"))
+                    {
+                        Canvas.SetLeft(x, Canvas.GetLeft(x) + ghostSpeed);
+                    }
+
+                    if (x.Name.Contains("down"))
+                    {
+                        Canvas.SetTop(x, Canvas.GetTop(x) + ghostSpeed);
+                    }
+
+                    // reduce one from the current ghost step integer
+                    currentGhostStep--;
+
+                    // if the current ghost step integer goes below 1 
+                    if (currentGhostStep < 1)
+                    {
+                        // reset the current ghost step to the ghost move step value
+                        currentGhostStep = ghostMoveStep;
+                        // reverse the ghost speed integer
+                        ghostSpeed = -ghostSpeed;
+                    }
+                }*/
+            }
+
+            if(score == 100)
+            {
+                GameOver($"You Win!. \nScore: {score}. \nClick ok to go to main menu");
+            }
+
+            if ((levelIime - DateTime.UtcNow).ToString(@"mm\:ss") == "00:00")
+            {
+                GameOver($"Time out!. \nScore: {score}. \nClick ok to go to main menu");
             }
         }
 
